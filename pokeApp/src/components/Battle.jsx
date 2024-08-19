e7f6a49f3d435ea6f5bf3148b5728a42930c6af3
mkalecsar <mkalecsar@gmail.com>
2024-08-19 10:37:55 +0200

    added: bootsrtap,component files, fetchData function

---
+++ pokeApp/src/components/Battle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DisplayBattle from './DisplayBattle'
+
+const Battle = () => {
+  return (
+    <>
+      <DisplayBattle/>
+      <div>BattleLogic</div>
+    </>
+    
+  )
+}
+
+export default Battle
